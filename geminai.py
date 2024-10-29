774424c1e8c0dc2c1a42846ef4553c89035369e7
Rosinês da Silva Almeida <r.da.silva.almeida@avanade.com>
2024-10-29 14:35:42 -0300

    add metodo selectLanguage

--- geminai.py
+++ geminai.py
@@ -2,27 +2,23 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
+#Configuração da chave de API
 load_dotenv()
 api_key = os.getenv("API_KEY")
 #TODO : REMOVER API KEY  
 #genai.configure(api_key="AIzaSyAoQsPcH8Yc0YWnl4bPTie3IjN_G96SLq4")
 genai.configure(api_key=api_key)
  
-# work
-# genai.configure(api_key="AIzaSyAoQsPcH8Yc0YWnl4bPTie3IjN_G96SLq4")
-
-# model = genai.GenerativeModel("gemini-1.5-flash")
-# response = model.generate_content("Escreva detalhe este codigo em  Python , quero que retorne em tópico 1-código, 2- descrição 3- exemplo: \n\n def add(a,b): \n return a+b")
-# print(response.text)
+ 
 
 # terminar o video 
 # ver documentação
 # ver se tem outro video 
-#ja criar seus comando nesse arquivo 
-# tentar passar para um pasta novo (ver como criar um projeto pythom )
+#ja criar seus comando nesse arquivo  
 
 # o que fazer com o codigo acima
-# - pedir pra usuário adicionar o linguagem de programação
+# - pedir pra usuário adicionar o linguagem de programação(selecionar dar opções de escolha com números )
+# - criar o input da pergunta
 # - pedir pra usuário adicionar o código
 #- fazer mockado estas informações priemiros
 # - dar exemplo de como eu quero o retono
@@ -34,14 +30,41 @@ genai.configure(api_key=api_key)
 
 #new test
 
+def selectLanguageUser(respostaInputLanguege):
+    if respostaInputLanguege == 1:
+        return 'Python'
+    if respostaInputLanguege == 2:
+        return 'Java'
+    if respostaInputLanguege == 3:
+        return 'JavaScript'
+    if respostaInputLanguege == 4:
+        return 'C#'
+    
 
-
+#Método para gerar a documentação
 model = genai.GenerativeModel("gemini-1.5-flash")
 def create_documentation(linguage, codigo):
     response = model.generate_content(f"Escreva detalhe este codigo em  {linguage} , quero que retorne em tópico 1-Código, 2- Descrição 3- Exemplo: \n\n {codigo}")
     return response.text
  
+# Dados do usuário  
+#linguage_code_user = selectLanguageUser(3)
+languageSelect = None
+while languageSelect is None:
+    try:
+        input_linguage_code_user = int(input("Digite o número correspondente à linguagem de programação desejada:\n1. Python\n2. Java\n3. JavaScript\n4. C#\n"))
+        languageSelect = selectLanguageUser(input_linguage_code_user)
+        
+        if languageSelect is None:
+            raise ValueError("Por favor, insira um número válido.")
 
-linguage_code_user = 'Python'
-codigo_usuario =  'def add(a,b): \n return a+b'
-response = print(create_documentation(linguage_code_user, codigo_usuario))
+        codigo_usuario = 'def add(a, b): \n return a + b'
+        response = print(create_documentation(languageSelect, codigo_usuario))
+    except ValueError:
+        print("Por favor, insira um número válido.")
+        
+
+
+
+
+
